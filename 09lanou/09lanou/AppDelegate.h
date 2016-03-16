3e05130534f2477c91e6bd602dd068f299e5c2b0
yjx <530572810@qq.com>
2016-03-16 10:24:52 +0800

    创建工程

---
+++ 09lanou/09lanou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  09lanou
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 BNUZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 09lanou/09lanou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  09lanou
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 BNUZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
